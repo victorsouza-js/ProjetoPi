cf0b17421dbb65f17c569f53ecb00a2d93eb53c6
victorsouza-git <victorsouzaaa5611@gmail.com>
2024-12-01 15:10:17 -0300

    add

--- projetopi/resources/views/admin/adm.blade.php
+++ projetopi/resources/views/admin/adm.blade.php
@@ -10,7 +10,7 @@
             src="{{ asset('animations/img.json') }}"
             background="transparent"
             speed="1"
-            style="width: 300px; height: 300px; margin-top: 11vh;"
+            style="width: 400px; height: 300px; margin-top: 11vh;"
             loop
             autoplay>
         </lottie-player>
