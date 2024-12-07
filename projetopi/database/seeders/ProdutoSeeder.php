b051b6d0c1e03f12ba65aa84149b59da349e180f
victorsouza-git <victorsouzaaa5611@gmail.com>
2024-12-07 08:44:34 -0300

    add

--- projetopi/database/seeders/ProdutoSeeder.php
+++ projetopi/database/seeders/ProdutoSeeder.php
@@ -25,6 +25,23 @@ class ProdutoSeeder extends Seeder
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
+            
+            [
+                'nome' => 'Creatina Dark Lab 300g',
+                'preco' => '73.00',
+                'qtd_estoque' => '670',
+                'categoria' => 'Monohidratada',
+                'status' => true,
+                'codigo_sku' => 'PROD123456',
+                'preco_promocional' => '60.00',
+                'peso' => '0.300',
+                'dimensoes' => '15x7x0.8 cm',
+                'fornecedor' => 'Tech Supply Ltda.',
+                'tags' => 'suplementos, creatinas, proteinas, monohidratadas',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+           
            
             
         ]);
